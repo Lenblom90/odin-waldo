3eee950dd5ead15b7e27f2478ca55a2005cbbd9b
lblom <lenblom@gmail.com>
2023-02-10 21:37:32 +0200

    Add backend and timer
    Linked App with firebase backend server to load data. Created timer and
    display message when user finishes.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,35 @@
 import './styles/App.css';
 import Box from './components/Box';
 import FoundBox from './components/FoundBox';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { firebaseConfig } from './firebase.config';
 
 export default function App() {
-  const characters = [{
-    id: 1,
-    name: "waldo",
-    coordsX: [630,730],
-    coordsY: [230,330]
-},
-{
-    id: 2,
-    name: "smoke",
-    coordsX: [325,425],
-    coordsY: [100,200]
-},
-{
-    id: 3,
-    name: "tower",
-    coordsX: [500,600],
-    coordsY: [80,180]
-},
-{
-    id: 4,
-    name: "boat",
-    coordsX: [75,300],
-    coordsY: [250,350]
-},
-{
-    id: 5,
-    name: "devil",
-    coordsX: [730,830],
-    coordsY: [180,280]
-}]
+  let characters = [];
+  useEffect(() => {  
+    const fetchData = async () => {
+      const app = initializeApp(firebaseConfig); 
+      const db = getFirestore(app);
+      const response =  await getDocs(collection(db, "characters"));
+      if(characters.length === 0){
+        response.forEach((element) => {
+          const character = element.data();
+          character.id = element.id;
+          characters.push(character);
+        });  
+      }
+    }
+    fetchData();
+  });
 
-const [options, setOptions] = useState(characters);
-const [hidden, setHidden] = useState(true);
-const [coords, setCoords] = useState([0,0]);
-const [found, setFound] = useState([]);
+  const [options, setOptions] = useState(characters);
+  const [hidden, setHidden] = useState(true);
+  const [coords, setCoords] = useState([0,0]);
+  const [found, setFound] = useState([]);
+  const [message, setMessage] = useState('');
+  const [time, setTime] = useState(new Date());
 
   const handleClick = (e) => {
     createBox(e.pageX - 50, e.pageY - 50);
@@ -51,15 +42,25 @@ const [found, setFound] = useState([]);
 
   const placeMarker = (char) => {
     setFound([...found, char]);
+    if(found.length === 4){
+      ShowScore(new Date() - time);
+    }
   }
- 
+
+  const ShowScore = (timespan) => {
+    const minutes = Math.round(timespan / 60000);
+    const seconds = Math.round((timespan % 60000) / 1000);
+    setMessage(`Congratulations! \n Your time is: ${minutes} minutes and ${seconds} seconds`);
+  }
+
   return (
     <div onClick={handleClick} className="App">
       <Box coords={coords} hidden={hidden} setHidden={setHidden} characters={options} setOptions={setOptions} placeMarker={placeMarker}/>      
       {found.map((char) => {
         const avgCoords = [(char.coordsX[0] + char.coordsX[1])/2, (char.coordsY[0] + char.coordsY[1])/2]
-        return <FoundBox coords={avgCoords} label={char.name} />
+        return <FoundBox key={char.id} coords={avgCoords} label={char.name} />
       })}
+      <div className='message'>{message}</div>
     </div>
   );
 }
